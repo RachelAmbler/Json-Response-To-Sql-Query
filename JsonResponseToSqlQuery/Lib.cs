77dfae970915d407e291bba373d7d9749b7d4c7a
Rachel Ambler <rambler@pureromance.com>
2021-03-31 21:57:11 -0400

    Add project handling Fixed issue where you couldn't ignore a wildcard object name

--- JsonResponseToSqlQuery/Lib.cs
+++ JsonResponseToSqlQuery/Lib.cs
@@ -8,7 +8,9 @@ namespace JsonResponseToSqlQuery
     {
         internal static void Error(string message)
         {
-            Console.Error.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Error.WriteLine($"\nError: {message}\n");
+            Console.ResetColor();
         }
       
     }
@@ -18,6 +20,24 @@ namespace JsonResponseToSqlQuery
         internal static string FixCaseOfName(this string text, char sep = '.')
         {
             return !text.Contains(sep) ? text : string.Join(sep, (text.StartsWith(sep)?text.Substring(1):text).Split(sep).Select(e => e.Length == 1 ? e.ToUpper() : FixCaseOfName((e.Substring(0, 1).ToUpper() + e.Substring(1)), '_').FixCaseOfName('-')));
+        }
+
+        internal static FileInfo ConvertFilePathToFileInfo(this string text)
+        {
+            FileInfo ret = null;
+            if (!string.IsNullOrEmpty(text))
+                ret = new FileInfo(text);
+
+            return ret;
+        }
+        
+        internal static FileInfo ConvertFilePathToFileInfo(this string text,  FileInfo referencedFile)
+        {
+            FileInfo ret = null;
+            if (!string.IsNullOrEmpty(text) && referencedFile != null)
+                ret = new FileInfo(Path.Join(referencedFile.DirectoryName, text));
+
+            return ret;
         }
 
         internal static string ReadAllText(this FileInfo file)
@@ -33,5 +53,28 @@ namespace JsonResponseToSqlQuery
             sw.Flush();
             sw.Close();
         }
+
+        
+        internal static FileInfo ReplacePath(this FileInfo file, FileInfo referencedFile)
+        {
+            return file == null ? null : new FileInfo(Path.Join(Directory.GetDirectoryRoot(referencedFile.DirectoryName ?? string.Empty), file.Name));
+        }
+
+        internal static string FileNameOnly(this string fullFilenameAndPath)
+        {
+            if (string.IsNullOrEmpty(fullFilenameAndPath))
+                return null;
+            var fi = new FileInfo(fullFilenameAndPath);
+            return fi.Name;
+        }
+
+        internal static string EnsurePathSsAbsolute(this string path)
+        {
+            if (string.IsNullOrEmpty(path))
+                return string.Empty;
+            return "";
+
+        }
+        
     }
 }
---
+++ JsonResponseToSqlQuery/SolutionFile.cs
@@ -0,0 +1,88 @@
+using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace JsonResponseToSqlQuery
+{
+    internal class SolutionFile
+    {
+        internal Root JsonRoot { get; private init; }
+        
+
+        internal SolutionFile(string jsonResponseFile,
+                string arrayName,
+                string jsonVariableName,
+                string defaultStringDataType,
+                string defaultFloatDataType,
+                string defaultDateDataType,
+                string defaultIntegerDataType,
+                string defaultUuidDataType,
+                string defaultInnerArrayDataType,
+                string innerArrayColumnNameSuffix,
+                string queryAliasName,
+                string sqlOutputFile,
+                string overrideMappingFile
+                )
+        {
+            JsonRoot = new Root
+            {
+                    DefaultResponseFileName = jsonResponseFile,
+                    ArrayName = arrayName,
+                    JsonVariableName = jsonVariableName,
+                    QueryAliasName = queryAliasName,
+                    SqlOutputFileName = sqlOutputFile,
+                    MappingFileName = overrideMappingFile,
+                    InnerArrayColumnNameSuffix = innerArrayColumnNameSuffix,
+                    DefaultDataTypes = new DefaultDataTypes
+                    {
+                            ArrayDateType = defaultInnerArrayDataType,
+                            StringDataType = defaultStringDataType,
+                            FloatDateType = defaultFloatDataType,
+                            DateDateType = defaultDateDataType,
+                            IntegerDateType = defaultIntegerDataType,
+                            UuidDateType = defaultUuidDataType
+                    }
+            };
+
+        }
+
+        private SolutionFile() { }
+
+        internal static SolutionFile Load(FileInfo solutionFile)
+        {
+            return new SolutionFile() {JsonRoot = JsonConvert.DeserializeObject<Root>(solutionFile.ReadAllText())};
+        }
+
+        internal void Save(FileInfo solutionFile)
+        {
+            JsonRoot.MappingFileName = JsonRoot.MappingFileName.FileNameOnly();
+            JsonRoot.DefaultResponseFileName = JsonRoot.DefaultResponseFileName.FileNameOnly();
+            JsonRoot.SqlOutputFileName = JsonRoot.SqlOutputFileName.FileNameOnly();
+            solutionFile.WriteAllText(JsonConvert.SerializeObject(JsonRoot, Formatting.Indented));
+        }
+        
+        internal class Root
+        {
+            [JsonProperty("defaultResponseFileName")] internal string DefaultResponseFileName { get; set; }
+            [JsonProperty("mappingFileName")] internal string MappingFileName { get; set; }
+            [JsonProperty("arrayName")] internal string ArrayName { get; set; }
+            [JsonProperty("jsonVariableName")] internal string JsonVariableName { get; set; }
+            [JsonProperty("innerArrayColumnNameSuffix")] internal string InnerArrayColumnNameSuffix { get; set; }
+            [JsonProperty("queryAliasName")] internal string QueryAliasName { get; set; }
+            [JsonProperty("sqlOutputFileName")] internal string SqlOutputFileName { get; set; }
+            
+            [JsonProperty("defaultDataTypes")] internal DefaultDataTypes DefaultDataTypes { get; set; }
+        }
+
+        internal class DefaultDataTypes
+        {
+            [JsonProperty("stringDataType")] internal string StringDataType { get; set; }
+            [JsonProperty("dateDataType")] internal string DateDateType  { get; set; }
+            [JsonProperty("floatDataType")] internal string FloatDateType  { get; set; }
+            [JsonProperty("integerDataType")] internal string IntegerDateType  { get; set; }
+            [JsonProperty("uuidDataType")] internal string UuidDateType  { get; set; }
+            [JsonProperty("arrayDataType")] internal string ArrayDateType  { get; set; }
+        }
+        
+    }
+}
